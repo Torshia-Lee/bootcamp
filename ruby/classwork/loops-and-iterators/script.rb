ab0ddcea5257bdbb90f48a59f5b7154f06c9b38c
Torshia Lee <torshwlee@gmail.com>
2024-10-29 12:25:24 -0500

    Pushing loops exercise

---
+++ ruby/classwork/loops-and-iterators/script.rb
@@ -0,0 +1,17 @@
+i = 1
+while i <= 20 do
+    if i % 2 == 0 
+        puts "#{i} is an even number"
+    else
+        puts "#{i} is an odd number"
+    end
+    i += 1
+end
+
+for i in 1..20 do
+    if i % 2 == 0 
+        puts "#{i} is an even number"
+    else
+        puts "#{i} is an odd number"
+    end
+end
